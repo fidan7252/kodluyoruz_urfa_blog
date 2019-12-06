a8d46e021e7ed2d89c06c29cec58eefe1abf9c81
edu <edu@youtube-hakanyalcinkaya.local>
2019-12-06 20:40:02 +0300

    logout added

--- blog_user/views.py
+++ blog_user/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 
 def login_user(request):
-    user_name = request.POST.get('email')
-    password = request.POST.get('password')
-    if user_name and password:
-        user = authenticate(
-            request, username=user_name,
-            password=password
-        )
-        print(user)
-        if user is not None:
-            login(request, user)
-            print('login oldun')
-        else:
-            print('login olamadin')
-    return render(request, 'blog_user/login.html', {})
+    if request.method == "POST":
+        user_name = request.POST.get('email')
+        password = request.POST.get('password')
+        if user_name and password:
+            user = authenticate(
+                request, username=user_name,
+                password=password
+            )
+            print(user)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                print('login olamadin')
+    return render(request, 'blog_user/login.html', {})\
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
--- py_blog/urls.py
+++ py_blog/urls.py
@@ -6,6 +6,7 @@ from blog_user import views as blog_user_views
 urlpatterns = [
     path('', blog_views.index, name='home'),
     path('login/', blog_user_views.login_user, name='login'),
+    path('logout/', blog_user_views.logout_user, name='logout'),
     path('category/<slug:cat_slug>/', blog_views.index, name='cat'),
     path('category/<slug:cat_slug>/<slug:post_slug>/', blog_views.post_detail, name='post_detail'),
     path('admin/', admin.site.urls),
